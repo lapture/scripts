988301b0986a94ae3aa33b51dc727d4f4491fd35
Danny <98533154+D4NNYRICHARDSON@users.noreply.github.com>
2023-07-25 11:06:03 +0100

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,18 @@
+    <!--LAPTURE SYSTEMS WEBSITE TO MC CORE SCRIPT-->
+    <script type="text/javascript" src="services.lapture.com/scripts/website-prompt.latest.js"></script>
+    <!--LAPTURE SYSTEMS SETTINGS SCRIPT-->
+    var settings = {
+        alertMessage: "You've landed on our website...  Click 'OK' to continue on our Mobile App.", // Your Message
+        username: "fashionuk", // Your Lapture Username
+        defaultMarketingChainLink: "welcome", // Default Marketing Chain Link
+        availableWebsitePages: ["location/", "docs/"], // Add Absolute Path
+        includeWebsitePath: true, // Include Every Path after /
+        campaigns: [
+          {
+            websitePage: ["contact"], // Add Absolute Path
+            marketingChainLink: "bio", // Default Marketing Chain Link
+            includeWebsitePath: false // Include Every Path after /
+          },
+          // Add More Campaign Objects...
+        ],
+      };
